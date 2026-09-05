86b033e72070d97dc9ac05c7d8cc147c12423941
ILYASSs <100728752+lilssayli22@users.noreply.github.com>
2026-09-05 22:54:31 +0200

    Clean up MonteCarlo.cpp by removing unused code
    Removed unused includes and the 'moyenner' function.

--- MonteCarlo.cpp
+++ MonteCarlo.cpp
@@ -4,14 +4,3 @@
 
 
 #include "MonteCarlo.h"
-
-#include <locale>
-
-
-double MonteCarlo ::moyenner(std :: vector <double> x) {
-    double result = 0.0;
-    for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++) {
-        result = result + x[*it];
-    }
-    return result;
-}
